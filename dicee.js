d35033dbec9b51e60afc92635fdde49f13a56cf0
Vivek kumar <vivekkr0311@gmail.com>
2021-02-11 11:59:07 +0530

    Normal game

--- dicee.js
+++ dicee.js
@@ -1,5 +1,4 @@
-var player_1 = prompt("Enter first player name !");
-var player_2 = prompt("Enter second player name !");
+
 
 var randomNumber1 = Math.floor(Math.random()*6 + 1);
 var randomNumber2 = Math.floor(Math.random()*6 + 1);
@@ -12,9 +11,9 @@ document.querySelectorAll(".dice")[0].lastElementChild.setAttribute("src", image
 document.querySelectorAll(".dice")[1].lastElementChild.setAttribute("src", image_source2);
 
 if(randomNumber1 > randomNumber2){
-    document.querySelector(".container").firstElementChild.innerHTML = player_1 + " won !";
+    document.querySelector(".container").firstElementChild.innerHTML =  "Player 1 won !";
 }else if(randomNumber2 > randomNumber1){
-    document.querySelector(".container").firstElementChild.innerHTML = player_2 + " won !";
+    document.querySelector(".container").firstElementChild.innerHTML =  "Player 2 won !";
 }else{
     document.querySelector(".container").firstElementChild.innerHTML = "Draw !";
 }
